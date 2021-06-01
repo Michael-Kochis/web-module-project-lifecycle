845c5faff667dfaf226bfde27d8cd45f9fe5f26a
Michael-Kochis <rhishisikk@gmail.com>
2021-06-01 15:59:50 -0500

    MVP, now to break into class components

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import { CardMain } from './components/cardMain';
+
+function App() {
+  return (
+    <div className="App">
+      <CardMain></CardMain>
+    </div>
+  );
+}
+
+export default App;
